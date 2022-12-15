edf291eabcc46671f53149bbecf268f2dd101b97
shin133334 <shin133334@gmail.com>
2022-12-16 00:36:30 +0900

    calans20

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,4 @@ Route::get('/posts/create', [PostController::class, 'create']);
 Route::get('/posts/{post}', [PostController::class, 'show']);
 Route::get('/posts/{post}/edit', [PostController::class, 'edit']);
 Route::put('/posts/{post}', [PostController::class, 'update']);
+Route::delete('/posts/{post}', [PostController::class, 'delete']);
